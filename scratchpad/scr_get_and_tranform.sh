89f3c973b5ee64e5e8bb84be7ee17173d5ff11b7
Marek Wyszyński <45630165+gitgeoman@users.noreply.github.com>
2023-08-09 21:35:24 +0200

    refactoring - get and transform in a single shot to save disk space

--- scratchpad/scr_get_and_tranform.sh
+++ scratchpad/scr_get_and_tranform.sh
@@ -1,36 +1,22 @@
 #!/bin/bash
 
-#dowlnoad file
-get_file() {
-  # echo ${item_name}
-  wget -P ./nrw "https://www.opengeodata.nrw.de/produkte/geobasis/lusat/dop/dop_jp2_f10/${i}.jp2"
-  echo "${i}" | tee -a log_download.txt # log ktore pliki juz pobrane
+get_file(){
+    if [ -f "./data/${i}.jp2" ]; then
+        echo "${i} już pobrano."
+    else
+        # echo "pobieram ${i}"
+        wget -P "./data/" "https://www.opengeodata.nrw.de/produkte/geobasis/lusat/dop/dop_jp2_f10/${i::-1}.jp2" && wait
+        gdalwarp -s_srs EPSG:25832 -q -tr 0.9 0.9 -tap -t_srs EPSG:3857 -co BIGTIFF=YES -co TILED=YES -co COMPRESS=LZW -of GTiff -r near "./data/${i::-1}.jp2" "./output/${i::-1}.tif" && wait
+        rm -rf "./nrw_2/${i::-1}.jp2"
+    fi
+
+
+    # echo "${i}" | tee -a log_download.txt # log ktore pliki juz pobrane
 
 }
-#gdal tranform
-transform() {
-  gdalwarp -s_srs EPSG:25832 -q -tr 0.9 0.9 -tap -t_srs EPSG:3857 -co BIGTIFF=YES -co TILED=YES -co COMPRESS=LZW -of GTiff -r near "./nrw/${i}.jp2" "./nrw_t/${i}.tif"
-}
 
-echo 'start skryptu'
-
-no_of_threats=15 # stacja 80
-
-# shellcheck disable=SC2013
+N=5 # stacja 80
 for i in $(cat filelist.txt); do
-  ((a = a % no_of_threats))
-  ((a++ == 0)) && wait
-  get_file "${i}" &
+    ((a=a%N)); ((a++==0)) && wait
+    get_file ${i} &
 done
-
-# shellcheck disable=SC2013
-for i in $(cat filelist.txt); do
-  ((a = a % no_of_threats))
-  ((a++ == 0)) && wait
-  transform "${i}" &
-done
-
-echo 'koniec skryptu'
-
-#unzip \*.zip
-# find . -name "*.zip" -exec rm -rf {} \;
